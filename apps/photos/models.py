99b233d2584288ab75eecfdde6ebc0b9f396f7c4
Zdravko <zdravko.galchov@gmail.com>
2025-06-14 15:25:53 +0100

    :construction: feat: add Photo model

--- apps/photos/models.py
+++ apps/photos/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+from apps.pets.models import Pet
+from apps.photos.validators import FileSizeValidator
+
+
+class Photo(models.Model):
+    photo = models.ImageField(
+        validators=[
+            FileSizeValidator(5),
+        ],
+        upload_to='files',
+    )
+    description = models.TextField(
+        max_length=300,
+        validators=[
+            MinLengthValidator(10),
+        ],
+        null=True,
+        blank=True,
+    )
+    location = models.CharField(
+        max_length=30,
+        null=True,
+        blank=True,
+    )
+    tagged_pets = models.ManyToManyField(
+        to=Pet,
+        blank=True,
+    )
+    date_of_publication = models.DateField(auto_now=True)
